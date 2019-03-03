4f722df87819e7eee9fc2c1b13035c438ca7670b
djwayne1985 <djwayne1985@gmail.com>
2019-03-03 00:34:52 +0000

    .

--- script.module.t2k-world-tv/addon.py
+++ script.module.t2k-world-tv/addon.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-#
-# Copyright (C) 2018 RACC
+## Copyright (C) 2018 RACC
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
